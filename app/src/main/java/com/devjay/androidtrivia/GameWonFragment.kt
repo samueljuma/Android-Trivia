5354a8f37e3aa279d81acdf2650642d8349f046b
samueljuma <samueljuma1024@gmail.com>
2021-05-22 23:47:16 +0300

    hide menu item for unresolved intents

--- app/src/main/java/com/devjay/androidtrivia/GameWonFragment.kt
+++ app/src/main/java/com/devjay/androidtrivia/GameWonFragment.kt
@@ -20,6 +20,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -43,14 +44,19 @@ class GameWonFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.winner_menu, menu)
+        //check if activity can be resolved
+        if(null == getShareIntent().resolveActivity(requireActivity().packageManager)){
+            // hide share menu
+            menu.findItem(R.id.share).setVisible(false)
+        }
     }
     // get the share intent
     private fun getShareIntent(): Intent{
         val args: GameWonFragmentArgs by navArgs()
         val shareIntent = Intent(Intent.ACTION_SEND)
-        shareIntent.setType("text/plain")
-            .putExtra(Intent.EXTRA_TEXT,
-                getString(R.string.share_success_text,args.numCorrect, args.numQuestions))
+            shareIntent.setType("text/plain")
+                .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text,
+                    args.numCorrect, args.numQuestions))
         return shareIntent
     }
     private fun shareSuccess(){
