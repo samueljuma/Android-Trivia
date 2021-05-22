9e60a307e2e6daa73db329b97d618e2503ae6fbb
samueljuma <samueljuma1024@gmail.com>
2021-05-22 22:59:06 +0300

    share menu/explicit intent

--- app/src/main/java/com/devjay/androidtrivia/GameWonFragment.kt
+++ app/src/main/java/com/devjay/androidtrivia/GameWonFragment.kt
@@ -16,10 +16,9 @@
 
 package com.devjay.androidtrivia
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -36,11 +35,32 @@ class GameWonFragment : Fragment() {
         binding.nextMatchButton.setOnClickListener{view: View ->
             view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
-        // note that we are not using val args = GameWonFragmentArgs.fromBundle(arguments)
-        val args: GameWonFragmentArgs by navArgs()
-        // test
-        Toast.makeText(context, "NumCorrect: ${args.numCorrect} " +
-                "NumQuestions ${args.numQuestions}",Toast.LENGTH_SHORT ).show()
+
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.winner_menu, menu)
+    }
+    // get the share intent
+    private fun getShareIntent(): Intent{
+        val args: GameWonFragmentArgs by navArgs()
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT,
+                getString(R.string.share_success_text,args.numCorrect, args.numQuestions))
+        return shareIntent
+    }
+    private fun shareSuccess(){
+       startActivity(getShareIntent())
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+           R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
