bc0c346a6d83e23d1f0bfff1347b899d1b5f6576
samueljuma <samueljuma1024@gmail.com>
2021-05-22 16:09:34 +0300

    implemented menu in fragment

--- app/src/main/java/com/devjay/androidtrivia/TitleFragment.kt
+++ app/src/main/java/com/devjay/androidtrivia/TitleFragment.kt
@@ -1,13 +1,13 @@
 package com.devjay.androidtrivia
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.devjay.androidtrivia.databinding.FragmentTitleBinding
 
 /**
@@ -25,7 +25,18 @@ class TitleFragment : Fragment() {
             // navigation can also create a listener
             Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
         )
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 
 }
