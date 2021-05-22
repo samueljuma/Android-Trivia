f2b661ca9ca93af153a41f983181b4f490aa35f9
samueljuma <samueljuma1024@gmail.com>
2021-05-22 17:03:24 +0300

    passing and accessing arguments across fragments

--- app/src/main/java/com/devjay/androidtrivia/GameWonFragment.kt
+++ app/src/main/java/com/devjay/androidtrivia/GameWonFragment.kt
@@ -20,9 +20,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
 import com.devjay.androidtrivia.databinding.FragmentGameWonBinding
 
 class GameWonFragment : Fragment() {
@@ -34,6 +36,11 @@ class GameWonFragment : Fragment() {
         binding.nextMatchButton.setOnClickListener{view: View ->
             view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
         }
+        // note that we are not using val args = GameWonFragmentArgs.fromBundle(arguments)
+        val args: GameWonFragmentArgs by navArgs()
+        // test
+        Toast.makeText(context, "NumCorrect: ${args.numCorrect} " +
+                "NumQuestions ${args.numQuestions}",Toast.LENGTH_SHORT ).show()
         return binding.root
     }
 }
