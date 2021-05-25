73d98afd61a5ac4a46bb6f410ac3489c53f41eb1
samueljuma <samueljuma1024@gmail.com>
2021-05-25 07:40:14 +0300

    complete navigation drawer

--- app/src/main/java/com/devjay/androidtrivia/MainActivity.kt
+++ app/src/main/java/com/devjay/androidtrivia/MainActivity.kt
@@ -3,22 +3,25 @@ package com.devjay.androidtrivia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.devjay.androidtrivia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-
+        // initialize drawer layout
+        drawerLayout = binding.drawerLayout
         val navController = this.findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this,navController)
-
+        NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView,navController)
     }
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController,drawerLayout)
     }
 }
